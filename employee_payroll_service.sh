582dc6f777d62ded615ea1c785634880e1c15d39
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-27 12:02:11 +0530

    Added: create a employee payroll table in the payroll service database

--- employee_payroll_service.sh
+++ employee_payroll_service.sh
@@ -28,4 +28,28 @@ mysql> select database();
 | payroll_service |
 +-----------------+
 1 row in set (0.00 sec)
-------------------------------------------------------------------
+------------------------------------------------------------------------------------------------
+
+#UC2:create a employee payroll table in the payroll service database to manage employee payrolls.
+
+mysql> create table employee_payroll
+    -> (
+    ->    id          int unsigned not null auto_increment,
+    ->    name        varchar(150) not null,
+    ->    salary      double not null,
+    ->    start       date not null,
+    ->    primary key (id)
+    -> );
+Query OK, 0 rows affected (0.04 sec)
+
+mysql> describe employee_payroll;
++--------+--------------+------+-----+---------+----------------+
+| Field  | Type         | Null | Key | Default | Extra          |
++--------+--------------+------+-----+---------+----------------+
+| id     | int unsigned | NO   | PRI | NULL    | auto_increment |
+| name   | varchar(150) | NO   |     | NULL    |                |
+| salary | double       | NO   |     | NULL    |                |
+| start  | date         | NO   |     | NULL    |                |
++--------+--------------+------+-----+---------+----------------+
+4 rows in set (0.01 sec)
+------------------------------------------------------------------------------------------------
