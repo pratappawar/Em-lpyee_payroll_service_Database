ae0d090c1aac52db62f85a5962fa5c9ffc109067
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-27 11:48:33 +0530

    Added: Create a Employee Payroll database

--- employee_payroll_service.sh
+++ employee_payroll_service.sh
@@ -1,1 +1,31 @@
 echo "Welcome to Employee PayRoll Service"
+
+#UC1
+
+mysql> create database payroll_service;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| payroll_service    |
+| performance_schema |
+| sakila             |
+| sys                |
+| world              |
++--------------------+
+7 rows in set (0.01 sec)
+
+mysql> use payroll_service;
+Database changed
+mysql> select database();
++-----------------+
+| database()      |
++-----------------+
+| payroll_service |
++-----------------+
+1 row in set (0.00 sec)
+------------------------------------------------------------------
